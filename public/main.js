f9f71c7354aa5c333412a7538df33265fcbaa957
willjw3 <will.j.ward3@gmail.com>
2020-03-22 13:28:56 +0900

    Removed unnecessary code

--- public/main.js
+++ public/main.js
@@ -1,10 +1,8 @@
-const puppeteer = require('puppeteer');
 const fetch = require("node-fetch");
 const fs = require("fs");
 
 const yesterdayCountryData = require("../src/data/country/history/covid19200320.json");
 const yesterdayProvinceData = require("../src/data/province/history/covid19200320.json")
-//const url = "https://ourworldindata.org/coronavirus";
 const url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-21-2020.csv";
 
 const getAllData = async () => {
@@ -107,7 +105,6 @@ const getProvinceList = async () => {
 
 const getCountryData = async() => {
     let countryData = [];
-    let countryDataArray = []
     let countries = await getCountryList()
     let allData = await getAllData();
     await countries.forEach(async(nation) => {
@@ -132,15 +129,11 @@ const getCountryData = async() => {
             console.log("Prolly bad")
         }
     })
-    // countryDataArray = JSON.stringify(countryData);
-    // fs.writeFileSync("src/data/country/covid19.json", countryDataArray);    
-    // fs.writeFileSync("src/data/country/history/covid19200320.json", countryDataArray); 
     return countryData;   
 }
 
 const getProvinceData = async() => {
     let provinceData = [];
-    let provinceDataArray = []
     let provinces = await getProvinceList()
     let allData = await getAllData();
     await provinces.forEach(async(province) => {
@@ -168,9 +161,6 @@ const getProvinceData = async() => {
             console.log("Prolly bad")
         }
     })
-    // provinceDataArray = JSON.stringify(provinceData);
-    // fs.writeFileSync("src/data/province/covid19.json", provinceDataArray);    
-    // fs.writeFileSync("src/data/province/history/covid19200320.json", provinceDataArray);
     return provinceData;    
 }
 
@@ -251,86 +241,8 @@ const makeProvinceDataFile = async () => {
     fs.writeFileSync("src/data/province/covid19.json", stringyProvinceCasesArray);    
     fs.writeFileSync("src/data/province/history/covid19200321.json", stringyProvinceCasesArray);
 }
-// getCountryData();
-// getProvinceData();
+
 getWorldTotal();
 makeCountryDataFile();
 makeProvinceDataFile();
-
-//getAllData();
-// async function scraper() {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-//     await page.goto(url);
-//     await page.waitForSelector(".location", {
-//         visible: true,
-//     });
-//     await page.evaluate(async () => {
-//         let locations = [];
-//         let doublingDays = [];
-//         let totalCases = [];
-//         let newCases = [];
-//         let data = [];
-//         let locationtd = await document.getElementById("covid-table-embed").getElementsByTagName("tbody")[0].getElementsByTagName("tr").length
-//         for (let i=0; i<locationtd; i++) {
-//             locations.push(document.getElementById("covid-table-embed").getElementsByTagName("tbody")[0].getElementsByClassName("location")[i].innerText)
-//         }
-//         for (let i=0; i<locationtd; i++) {
-//             doublingDays.push(document.getElementById("covid-table-embed").getElementsByTagName("tbody")[0].getElementsByClassName("doubling-days")[i].getElementsByTagName("span")[1].innerText)
-//         }
-//         for (let i=0; i<locationtd; i++) {
-//             newCases.push(document.getElementById("covid-table-embed").getElementsByTagName("tbody")[0].getElementsByClassName("new-cases")[i].getElementsByClassName("value")[0].getElementsByTagName("span")[0].innerText)
-//         }
-//         for (let i=0; i<locationtd; i++) {
-//             totalCases.push(document.getElementById("covid-table-embed").getElementsByTagName("tbody")[0].getElementsByClassName("total-cases")[i].getElementsByClassName("value")[0].getElementsByTagName("span")[0].innerText)
-//         }
-//         await locations.forEach((location, index) => {
-//             let countryData = {
-//                 "location": location,
-//                 "doubling_days": doublingDays[index],
-//                 "total_cases": totalCases[index],
-//                 "new_cases": newCases[index]
-//             }
-//             data.push(countryData)
-//         });
-
-//         return data
-
-//     }).then(data => {
-//         //console.log(data)
-//         let dataList = JSON.stringify(data)
-//         fs.writeFileSync("src/corona.json", dataList)
-//         fs.writeFileSync("src/data/corona200320.json", dataList)
-//     }).catch(console.log("Faux-Error!"))
-// }
-
-// scraper()
-
-// await countries.forEach(async (nation) => {
-//     let confirmedCount = 0;
-//     let deathCount = 0;
-//     await rows.forEach(row => {
-//         let country = row.split(",");
-//         if (row.split(",").length === 8) {
-//             if (nation === country[1]) {
-//                 confirmedCount = confirmedCount + country[3];
-//                 deathCount = deathCount + country[4];
-//             }
-//         } else {
-//             if (nation === `${country[1].replace(/"/, "")}, ${country[2].replace(/"/, "")}`) {
-//                 confirmedCount = confirmedCount + country[4];
-//                 deathCount = deathCount + country[5];
-//             }
-//         }
-//     })
-//     countryData.push({
-//         country: nation,
-//         confirmed: confirmedCount,
-//         deaths: deathCount
-//     })
-// })
-// setTimeout(() => {
-//     console.log(countryData)
-// }, 5000)
-
 
