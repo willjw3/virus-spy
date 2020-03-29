77efe477e66d19a57503fa76124ea3790099900f
willjw3 <will.j.ward3@gmail.com>
2020-03-29 20:29:48 +0900

    Added world map component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,15 @@
-import React from "react"
-import CountryTable from "../components/countryTable"
-import Header from "../components/header"
-import "../styles/home.scss"
+import React from "react";
+import CountryTable from "../components/countryTable";
+import WorldMap from "../components/worldmap"
+import Header from "../components/header";
+import "../styles/home.scss";
 
 
 const Home = () => {
     return (
         <div className="home">
             <Header />
+            {/* <WorldMap /> */}
             <CountryTable />
         </div>
     )
